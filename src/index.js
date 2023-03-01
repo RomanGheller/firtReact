1c47018bf49bba64de0f9a7336609c6ca0fb0df3
Roman Gheller <roman.gheller@iis.utm.md>
2023-03-01 09:58:34 +0200

    My first Layout

--- src/index.js
+++ src/index.js
@@ -1,17 +1,101 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+ import React from "react"
+ import * as ReactDOMClient from 'react-dom/client'
+ import  ReactDOM from "react-dom"
+// // ReactDOM.render(
+// //    /* <div>
+// //     <h1> привет! </h1>,
+// //     <h2> hello! </h2>
+// //     </div>,*/
+// //     React.createElement( 'input',{
+// //         placeholder: "Help text",
+// //         onClick : () => console.log("Click"),
+// //         onMouseEnter : () => console.log("Mouse")
+// //     }),
+// //     document.getElementById("app")
+// // )
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+// const inputClick = () => console.log("Click")
+// const mouseOver = () => console.log("Mouse")
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// const helpText = "Help text!"
+
+
+
+// const app = ReactDOMClient.createRoot(document.getElementById("app"))
+// app.render(elem)
+
+//import React from 'react';
+import { Layout, Space } from 'antd';
+
+const { Header, Footer, Sider, Content } = Layout;
+
+const headerStyle  = {
+  textAlign: 'center',
+  color: '#fff',
+  height: 64,
+  paddingInline: 50,
+  lineHeight: '64px',
+  backgroundColor: '#7dbcea',
+};
+
+const contentStyle = {
+  textAlign: 'center',
+  minHeight: 120,
+  lineHeight: '120px',
+  color: '#fff',
+  backgroundColor: '#108ee9',
+};
+
+const siderStyle = {
+  textAlign: 'center',
+  lineHeight: '120px',
+  color: '#fff',
+  backgroundColor: '#3ba0e9',
+};
+
+const footerStyle =  {
+  textAlign: 'center',
+  color: '#fff',
+  backgroundColor: '#7dbcea',
+};
+
+const elem = (
+
+  <Space direction="vertical" style={{ width: '100%' }} size={[0, 48]}>
+    <Layout>
+      <Header style={headerStyle}>Header</Header>
+      <Content style={contentStyle}>Content</Content>
+      <Footer style={footerStyle}>Footer</Footer>
+    </Layout>
+    <Layout>
+      <Header style={headerStyle}>Header</Header>
+      <Layout>
+        <Sider style={siderStyle}>Sider</Sider>
+        <Content style={contentStyle}>Content</Content>
+      </Layout>
+      <Footer style={footerStyle}>Footer</Footer>
+    </Layout>
+    <Layout>
+      <Header style={headerStyle}>Header</Header>
+      <Layout>
+        <Content style={contentStyle}>Content</Content>
+        <Sider style={siderStyle}>Sider</Sider>
+      </Layout>
+      <Footer style={footerStyle}>Footer</Footer>
+    </Layout>
+    <Layout>
+      <Sider style={siderStyle}>Sider</Sider>
+      <Layout>
+        <Header style={headerStyle}>Header</Header>
+        <Content style={contentStyle}>Content</Content>
+        <Footer style={footerStyle}>Footer</Footer>
+      </Layout>
+    </Layout>
+  </Space>
+
+)
+
+ const app = ReactDOMClient.createRoot(document.getElementById("App"))
+ app.render(elem)
+
+//export default App;
